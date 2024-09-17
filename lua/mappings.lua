d8b3f0d60277014c489b7b9ecf5af74aea7aed07
Abner <qw0207060413@gmail.com>
2024-09-17 14:40:29 +0800

    add nvim tree width mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,6 +14,8 @@ map("n", "<leader>fd", function()
   vim.diagnostic.open_float { border = "rounded" }
 end, { desc = "floating diagnostic" })
 map("n", "<leader>mp", "<cmd> MarkdownPreview <CR>", { desc = "toggle markdown preview server" })
+map("n", "<M-j>", "<cmd> NvimTreeResize +10 <CR>", {desc = "increase nvim tree width"})
+map("n", "<M-k>", "<cmd> NvimTreeResize -10 <CR>", {desc = "decrease nvim tree width"})
 
 -- visual mode
 map("x", "<", "<gv", { desc = "indent backward and stay in visual mode" })
