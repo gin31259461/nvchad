94fb03f2d7e759baedf1899c21659f45ebb5cbe4
Abner <qw0207060413@gmail.com>
2025-03-18 15:14:43 +0800

    update modules and modify eslint_d linter setting

--- lua/configs/nvim-lint-opt.lua
+++ lua/configs/nvim-lint-opt.lua
@@ -1,3 +1,5 @@
+local eslint_d_binary_name = "eslint_d"
+
 local options = {
   events = { "BufWritePost", "BufReadPost", "InsertLeave" },
 
@@ -20,6 +22,26 @@ local options = {
 
   linters = {
     eslint_d = {
+
+      cmd = function()
+        local local_binary = vim.fn.fnamemodify("./node_modules/.bin/" .. eslint_d_binary_name, ":p")
+        return vim.loop.fs_stat(local_binary) and local_binary or eslint_d_binary_name
+      end,
+
+      stdin = true,
+
+      stream = "stdout",
+
+      ignore_exitcode = true,
+
+      parser = function(output, bufnr)
+        local result = require("lint.linters.eslint").parser(output, bufnr)
+        for _, d in ipairs(result) do
+          d.source = eslint_d_binary_name
+        end
+        return result
+      end,
+
       args = {
         "--no-warn-ignored", -- <-- this is the key argument
         "--stdin",
