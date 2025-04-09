2dceead3195e5619592a227c99a34c240a93c47e
Abner <qw0207060413@gmail.com>
2025-04-09 21:03:38 +0800

    update config

--- lua/configs/lspconfig-config.lua
+++ lua/configs/lspconfig-config.lua
@@ -2,23 +2,24 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-local lspconfig_opt = require "configs.lspconfig-opt"
+local lspconfig_opts = require "configs.lspconfig-opt"
 local nvlsp = require "nvchad.configs.lspconfig"
 local pkgs = require("configs.packages").lsp_servers
 
-for _, lsp_server_name in ipairs(pkgs) do
-  if lspconfig_opt.setup[lsp_server_name] ~= nil then
-    lspconfig[lsp_server_name].setup {
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-    }
-  else
-    lspconfig[lsp_server_name].setup {
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-      on_attach = nvlsp.on_attach,
-    }
+for _, server in ipairs(pkgs) do
+  local servers = lspconfig_opts.servers
+  local server_opts = vim.tbl_deep_extend("force", {
+    on_init = vim.deepcopy(nvlsp.on_init),
+    capabilities = vim.deepcopy(nvlsp.capabilities),
+  }, servers[server] or {})
+
+  if lspconfig_opts.setup[server] == nil then
+    server_opts = vim.tbl_deep_extend("force", {
+      on_attach = vim.deepcopy(nvlsp.on_attach),
+    }, server_opts or {})
   end
+
+  lspconfig[server].setup(server_opts)
 end
 
 lspconfig.denols.setup {
