7d63d401ee9d0a7fcfcec8c5a49f7c92a09b2e4b
Abner <qw0207060413@gmail.com>
2025-08-04 15:35:06 +0800

    chore: filter signature help notify

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -340,6 +340,17 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+      -- control msgs
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "signature help",
+          },
+          opts = { skip = true },
+        },
+      },
+
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
@@ -347,8 +358,12 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
+
+        hover = {
+          silent = true,
+        },
       },
-      -- you can enable a preset for easier configuration
+
       presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
@@ -370,7 +385,7 @@ return {
   {
     "folke/trouble.nvim",
     lazy = false,
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {},
     cmd = "Trouble",
     keys = {
       {
@@ -475,6 +490,7 @@ return {
 
             { path = "lazy.nvim", mods = { "lazy" } },
             { path = "snacks.nvim", words = { "snacks", "snacks.nvim" } },
+            { path = "noice.nvim", words = { "noice", "noice.nvim" } },
           },
         },
       },
