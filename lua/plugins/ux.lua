6dd23a3d30a5a60a13d3eb177872f7e4f4e71a47
Abner <qw0207060413@gmail.com>
2025-08-06 12:35:53 +0800

    update: setting indent blank line config

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -531,4 +531,24 @@ return {
       return require("configs.cmp-opt")
     end,
   },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "User FilePost",
+    ---@type ibl.config
+    opts = {
+      indent = { char = "│", highlight = "IblChar" },
+      scope = { char = "│", highlight = "IblScopeChar" },
+    },
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "blankline")
+
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      require("ibl").setup(opts)
+
+      dofile(vim.g.base46_cache .. "blankline")
+    end,
+  },
 }
