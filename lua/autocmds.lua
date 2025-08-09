b3d40832432dd6af674c133e38fde3159df53eb1
Abner <qw0207060413@gmail.com>
2025-08-09 16:12:44 +0800

    feat: add user cmd ClearShada for windows

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,3 +6,29 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
     end
   end,
 })
+
+-- FIX (temp): issue: https://github.com/neovim/neovim/issues/8587
+-- this method is from: https://github.com/neovim/neovim/issues/8587#issuecomment-2176399196
+if NvChad.shell.is_win() then
+  vim.api.nvim_create_user_command("ClearShada", function()
+    local shada_path = vim.fn.expand(vim.fn.stdpath("data") .. "/shada")
+    local files = vim.fn.glob(shada_path .. "/*", false, true)
+    local all_success = 0
+    for _, file in ipairs(files) do
+      local file_name = vim.fn.fnamemodify(file, ":t")
+      if file_name == "main.shada" then
+        -- skip your main.shada file
+        goto continue
+      end
+      local success = vim.fn.delete(file)
+      all_success = all_success + success
+      if success ~= 0 then
+        vim.notify("Couldn't delete file '" .. file_name .. "'", vim.log.levels.WARN)
+      end
+      ::continue::
+    end
+    if all_success == 0 then
+      vim.print("Successfully deleted all temporary shada files")
+    end
+  end, { desc = "Clears all the .tmp shada files" })
+end
