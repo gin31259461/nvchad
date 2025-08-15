8439ffd48c9177deaacd204acfa00b36955493c4
Abner <qw0207060413@gmail.com>
2025-08-16 00:10:01 +0800

    disable tiny-inline-diagnostic

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,6 +3,7 @@ return {
   {
     -- config: https://github.com/rachartier/tiny-inline-diagnostic.nvim?tab=readme-ov-file#%EF%B8%8F-setup
     "rachartier/tiny-inline-diagnostic.nvim",
+    cond = false,
     event = "VeryLazy",
     priority = 1000, -- needs to be loaded in first
     opts = {
