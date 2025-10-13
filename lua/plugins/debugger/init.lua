7cd0c406a57f11e6292d791debe49af2de183fe2
Abner <qw0207060413@gmail.com>
2025-10-13 15:40:00 +0800

    update(dap): don't auto open dap ui

--- lua/plugins/debugger/init.lua
+++ lua/plugins/debugger/init.lua
@@ -61,18 +61,18 @@ return {
 
       dapui.setup(opts)
 
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
+      -- dap.listeners.before.attach.dapui_config = function()
+      --   dapui.open()
+      -- end
+      -- dap.listeners.before.launch.dapui_config = function()
+      --   dapui.open()
+      -- end
+      -- dap.listeners.before.event_terminated.dapui_config = function()
+      --   dapui.close()
+      -- end
+      -- dap.listeners.before.event_exited.dapui_config = function()
+      --   dapui.close()
+      -- end
 
       vim.keymap.set("n", "<leader>du", function()
         dapui.toggle()
