291dcd2bda6e13f0225e2600416898e73440d970
Abner <qw0207060413@gmail.com>
2026-04-20 19:14:29 +0800

    update plugins

--- lua/plugins/ui/dotnet.lua
+++ lua/plugins/ui/dotnet.lua
@@ -2,6 +2,7 @@
 return {
   {
     "gin31259461/dotnet-cli.nvim",
+    dependencies = { "gin31259461/comet.nvim" },
     cmd = { "DotnetManager", "DotnetBuild", "DotnetPublish", "DotnetGlobalJson" },
     ft = "cs",
     opts = {},
