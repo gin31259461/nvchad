0b241ba347958d23fe88d17b7a791cf6ae87ede5
abner <qw0207060413@gmail.com>
2026-05-25 10:39:53 +0800

    feat: edgy.nvim plugin, wrap windows of trouble.nvim

--- lua/plugins/ui/edgy.lua
+++ lua/plugins/ui/edgy.lua
@@ -8,17 +8,53 @@ return {
       vim.opt.laststatus = 3
       vim.opt.splitkeep = "screen"
     end,
-    opts = {
-      close_when_all_hidden = false,
-
-      ---@type (Edgy.View.Opts|string)[]
-      right = {
-        {
-          ft = "trouble",
-          size = { width = 0.3 },
-          pinned = true,
-        },
+    keys = {
+      {
+        "<leader>ue",
+        function()
+          require("edgy").toggle()
+        end,
+        desc = "Edgy Toggle",
+      },
+      {
+        "<leader>uE",
+        function()
+          require("edgy").select()
+        end,
+        desc = "Edgy Select Window",
       },
     },
+    opts = function()
+      local opts = {
+        ---@type (Edgy.View.Opts|string)[]
+        bottom = {},
+
+        animate = {
+          enabled = false,
+        },
+      }
+
+      -- luacheck: ignore
+      -- ref: https://github.com/LazyVim/LazyVim/blob/fa88241e2f633feb530b09dc014fc51dcff5f5a8/lua/lazyvim/plugins/extras/ui/edgy.lua#L99
+      for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+        opts[pos] = opts[pos] or {}
+        ---@type Edgy.View.Opts
+        local new_opts = {
+          ft = "trouble",
+          filter = function(_, win)
+            return vim.w[win].trouble
+              and vim.w[win].trouble.position == pos
+              and vim.w[win].trouble.type == "split"
+              and vim.w[win].trouble.relative == "editor"
+              and not vim.w[win].trouble_preview
+          end,
+          size = (pos == "left" or pos == "right") and { width = 0.3 }
+            or { height = 0.3 },
+        }
+        table.insert(opts[pos], new_opts)
+      end
+
+      return opts
+    end,
   },
 }
--- lua/plugins/ui/trouble.lua
+++ lua/plugins/ui/trouble.lua
@@ -1,39 +1,73 @@
 ---@type LazySpec[]
 return {
   {
+    -- ref: https://github.com/folke/trouble.nvim/blob/main/docs/examples.md
     "folke/trouble.nvim",
     lazy = false,
-    opts = {},
+
+    opts = {
+
+      ---@type table<string, trouble.Mode>
+      modes = {
+        ---@diagnostic disable-next-line
+        diagnostic_preview_float = {
+          mode = "diagnostics",
+          preview = {
+            type = "float",
+            relative = "editor",
+            border = "rounded",
+            title = "Preview",
+            title_pos = "center",
+            position = { 0, -2 },
+            size = { width = 0.3, height = 0.3 },
+            zindex = 200,
+          },
+        },
+      },
+    },
     cmd = "Trouble",
     keys = {
       {
-        "<leader>tx",
-        "<cmd>Trouble diagnostics toggle<cr>",
+        "<leader>xd",
+        "<cmd>Trouble diagnostic_preview_float toggle win.position=bottom<cr>",
         desc = "Diagnostics (Trouble)",
       },
       {
-        "<leader>tX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        "<leader>xD",
+        "<cmd>Trouble diagnostic_preview_float toggle filter.buf=0 win.position=bottom<cr>",
         desc = "Buffer Diagnostics (Trouble)",
       },
       {
         "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
+        "<cmd>Trouble symbols toggle focus=false win.position=right<cr>",
         desc = "Symbols (Trouble)",
       },
       {
         "<leader>cL",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        function()
+          local trouble = require("trouble")
+
+          ---@diagnostic disable-next-line
+          trouble.close({ mode = "lsp" })
+          trouble.open({
+            mode = "lsp",
+            auto_refresh = false,
+            win = {
+              position = "right",
+            },
+            focus = false,
+          })
+        end,
         desc = "LSP Definitions / references / ... (Trouble)",
       },
       {
-        "<leader>tL",
-        "<cmd>Trouble loclist toggle<cr>",
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle win.position=bottom<cr>",
         desc = "Location List (Trouble)",
       },
       {
-        "<leader>tQ",
-        "<cmd>Trouble qflist toggle<cr>",
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle win.position=bottom<cr>",
         desc = "Quickfix List (Trouble)",
       },
     },
