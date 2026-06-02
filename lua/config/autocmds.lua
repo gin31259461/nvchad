87fc6c9b010bf1cad8c87b0fc540048441e8a95f
abner <qw0207060413@gmail.com>
2026-06-03 01:15:49 +0800

    feat: auto cd dir when 'nvim dir' is called

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,21 @@ vim.api.nvim_create_autocmd("DiagnosticChanged", {
   end,
   desc = "Redraw statusline when diagnostics change",
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("AutoSetDir", { clear = true }),
+  callback = function(data)
+    if vim.fn.isdirectory(data.file) ~= 1 then
+      return
+    end
+
+    vim.api.nvim_set_current_dir(data.file)
+
+    vim.schedule(function()
+      if vim.api.nvim_buf_is_valid(data.buf) then
+        vim.api.nvim_buf_delete(data.buf, { force = true })
+      end
+    end)
+  end,
+  desc = "cd into directory when `nvim <dir>` is used",
+})
